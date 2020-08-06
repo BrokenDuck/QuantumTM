42508f4b85541113401e3bc9749aeba5f6e87e1b
BrokenDuck <40206399+BrokenDuck@users.noreply.github.com>
2020-08-06 22:10:34 +0200

    Updates host program

--- host.py
+++ host.py
@@ -2,8 +2,8 @@ import qsharp
 
 from GraphColoring import graphColoringMain
 
-vertices = 5
-edges = [(0, 1), (0, 2), (1, 4), (2, 4)]
+vertices = 7
+edges = [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (3, 5), (2, 6), (5, 6), (4, 6)]
 
 coloring = graphColoringMain.simulate(V = vertices, edges = edges)
 
